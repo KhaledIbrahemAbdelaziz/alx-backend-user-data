b82b45c39bcd593d064c51840c0e2fca75b19e87
KhaledIbrahemAbdelaziz <kk6673326@gmail.com>
2024-01-24 16:42:28 -0800

    Task 2

--- 0x03-user_authentication_service/db.py
+++ 0x03-user_authentication_service/db.py
@@ -8,6 +8,9 @@ from sqlalchemy.orm.exc import NoResultFound
 from user import Base, User
 from typing import TypeVar
 
+VALID_FIELDS = ['id', 'email', 'hashed_password', 'session_id',
+                'reset_token']
+
 class DB:
     """DB class."""
 
@@ -33,3 +36,13 @@ class DB:
         session.add(user)
         session.commit()
         return user
+
+    def find_user_by(self, **kwargs) -> User:
+        """Find the user by any item in the database"""
+        if not kwargs or any(x not in VALID_FIELDS for x in kwargs):
+            raise InvalidRequestError
+        session = self._session
+        try:
+            return session.query(User).filter_by(**kwargs).one()
+        except Exception:
+            raise NoResultFound
