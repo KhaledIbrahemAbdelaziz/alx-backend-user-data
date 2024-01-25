5bef14d7c4b120f99e959483aaa362826a08cb24
KhaledIbrahemAbdelaziz <kk6673326@gmail.com>
2024-01-24 16:21:16 -0800

    Task 1

--- 0x03-user_authentication_service/db.py
+++ 0x03-user_authentication_service/db.py
@@ -3,35 +3,35 @@
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
-from sqlalchemy.orm.session import Session
-
-from user import Basemodel, User
-
+from sqlalchemy.exc import InvalidRequestError
+from sqlalchemy.orm.exc import NoResultFound
+from user import Base, User
+from typing import TypeVar
 
 class DB:
-    """DB class"""
+    """DB class."""
 
-    def __init__(self) -> None:
-        """Initialize a new DB instance"""
-        self._engine = create_engine("sqlite:///a.db", echo=True)
-        Basemodel.metadata.drop_all(self._engine)
-        Basemodel.metadata.create_all(self._engine)
+    def __init__(self):
+        """Constructor."""
+        self._engine = create_engine("sqlite:///a.db", echo=False)
+        Base.metadata.drop_all(self._engine)
+        Base.metadata.create_all(self._engine)
         self.__session = None
 
     @property
-    def _session(self) -> Session:
-        """Memoized session object"""
+    def _session(self):
+        """_session."""
         if self.__session is None:
             DBSession = sessionmaker(bind=self._engine)
             self.__session = DBSession()
         return self.__session
 
     def add_user(self, email: str, hashed_password: str) -> User:
-        """Adding a new User object to the database"""
+        """add a new User object to the database."""
         if not email or not hashed_password:
             return
-        users = User(email=email, hashed_password=hashed_password)
-        sessions = self._session
-        sessions.add(users)
-        sessions.commit()
-        return users
+        user = User(email=email, hashed_password=hashed_password)
+        session = self._session
+        session.add(user)
+        session.commit()
+        return user
--- 0x03-user_authentication_service/user.py
+++ 0x03-user_authentication_service/user.py
@@ -4,10 +4,10 @@ from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy import Column, Integer, String
 
 
-Basemodel = declarative_base()
+Base = declarative_base()
 
 
-class User(Basemodel):
+class User(Base):
     """User Class Reperesntation"""
     __tablename__ = 'users'
     id = Column(Integer, primary_key=True)
